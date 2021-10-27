ca00d2f531621b848f744d4622f24d2062d0a88f
Khushbu Kumari <kumari.k@northeastern.edu>
2021-10-26 19:34:59 -0700

    Midterm Que2.solution for implementing singleton design.

---
+++ Midterm/SingletonDesign/src/question2solution/Main.java
@@ -0,0 +1,12 @@
+package question2solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer p1;
+        // refers to the only object of Database
+        p1= Printer.getInstance();
+        p1.getConnection();
+    }
+}
+
